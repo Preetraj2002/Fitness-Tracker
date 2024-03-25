aa4ac943a339c61741ed927aaa432f551b50ddaa
CHANDNI JHA <96429544+ChandniJha630@users.noreply.github.com>
2024-03-25 13:56:20 +0530

    Update profilePage.py

--- FrontEnd/profilePage.py
+++ FrontEnd/profilePage.py
@@ -1,23 +1,5 @@
 import streamlit as st
 from PIL import Image
-# Sample user data (replace with actual user data)
-# user_data = {
-#     "username": "JohnDoe",
-#     "bmi": 25.5  # Example BMI value
-# }
-
-# def show_profile(user_id, conn):
-#     cur = conn.cursor()
-#     cur.execute('SELECT name, sex, age, weight_kg, height_cm, bmi FROM "user" WHERE id = :user_id', {'user_id': user_id})
-#     user_data = cur.fetchone()
-#     st.title("Profile")
-#     st.write(f"Name: {user_data[0]}")
-#     st.write(f"Sex: {user_data[1]}")
-#     st.write(f"Age: {user_data[2]}")
-#     st.write(f"Weight (kg): {user_data[3]}")
-#     st.write(f"Height (cm): {user_data[4]}")
-#     st.write(f"BMI: {user_data[5]}")
-
 
 def edit_profile(user_id, conn):
     cur = conn.cursor()
@@ -56,31 +38,38 @@ def show_profile(user_id, conn):
     im=Image.open('Asset/runner.png') 
     # Streamlit app layout
    # Assuming 'im' is your image variable
-    col1, col2 = st.columns([1, 5])
+    col1, col2 = st.columns([1, 9])
+   
+    with col2:
+        st.title("Profile")
     with col1:
         # Display the grayscale image
         st.image(im, width=100)
-
-    with col2:
-        st.title("Profile")
     cur = conn.cursor()
     cur.execute(
         'SELECT name, sex, age, weight_kg, height_cm, bmi FROM "user" WHERE id = :user_id',
         {"user_id": user_id},
     )
     user_data = cur.fetchone()
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        if user_data[1]=='male':
+                st.image('Asset/male.png', width=300)
+        else:
+                st.image('Asset/female.png', width=300)
+    with col2:
 
-    edit_mode = st.button("Edit Profile")
+        edit_mode = st.button("Edit Profile")
 
-    if edit_mode:
-        edit_profile(user_id, conn)
-    else:
-        info_style = "font-size: 18px; padding: 10px; background-color: black; border-radius: 5px;"
-
-        st.write(f"<p style='{info_style}'><b>Name:</b> {user_data[0]}</p>", unsafe_allow_html=True)
-        st.write(f"<p style='{info_style}'><b>Sex:</b> {user_data[1]}</p>", unsafe_allow_html=True)
-        st.write(f"<p style='{info_style}'><b>Age:</b> {user_data[2]}</p>", unsafe_allow_html=True)
-        st.write(f"<p style='{info_style}'><b>Weight (kg):</b> {user_data[3]}</p>", unsafe_allow_html=True)
-        st.write(f"<p style='{info_style}'><b>Height (cm):</b> {user_data[4]}</p>", unsafe_allow_html=True)
-        st.write(f"<p style='{info_style}'><b>BMI:</b> {user_data[5]}</p>", unsafe_allow_html=True)
-
+        if edit_mode:
+            edit_profile(user_id, conn)
+        else:
+            # Styling individual data points
+            info_style = "font-size: 18px; padding: 10px; background-color: black; border-radius: 5px;"
+        
+            st.write(f"<p style='{info_style}'><b>Name:</b> {user_data[0]}</p>", unsafe_allow_html=True)
+            st.write(f"<p style='{info_style}'><b>Sex:</b> {user_data[1]}</p>", unsafe_allow_html=True)
+            st.write(f"<p style='{info_style}'><b>Age:</b> {user_data[2]}</p>", unsafe_allow_html=True)
+            st.write(f"<p style='{info_style}'><b>Weight (kg):</b> {user_data[3]}</p>", unsafe_allow_html=True)
+            st.write(f"<p style='{info_style}'><b>Height (cm):</b> {user_data[4]}</p>", unsafe_allow_html=True)
+            st.write(f"<p style='{info_style}'><b>BMI:</b> {user_data[5]}</p>", unsafe_allow_html=True)
