89780b4712d9e60dcbdf24f1947d952bc4681470
CHANDNI JHA <96429544+ChandniJha630@users.noreply.github.com>
2024-03-25 11:39:28 +0530

    Update display_navigation.py UI-UX of sidebar updated

--- FrontEnd/display_navigation.py
+++ FrontEnd/display_navigation.py
@@ -6,7 +6,7 @@ from FrontEnd.sleepPage import sleep_quality_tracker
 from FrontEnd.profilePage import show_profile
 from FrontEnd.waterPage import water_intake_tracker
 from FrontEnd.loginPage import user_login
-
+from streamlit_option_menu import option_menu
 
 def connect_to_database1():
     connStr = "system/prakriti1@localhost:1521/xepdb1"
@@ -14,29 +14,38 @@ def connect_to_database1():
     return conn
 
 
+from PIL import Image
+im=Image.open('Asset/logo.png')
+
 def navigation(user_id, conn):
     # If user is logged in, show the selected page
     if user_id:
         st.empty()
-        selection = st.sidebar.radio(
-            "Go to",
-            [
-                "Food Calorie Tracker",
-                "Sleep Quality Tracker",
-                "Profile",
-                "Water Intake Tracker",
-                "Logout",
-            ],
-        )
-        if selection == "Food Calorie Tracker":
-            food_calorie_tracker(user_id, conn)
-        elif selection == "Sleep Quality Tracker":
-            sleep_quality_tracker(user_id, conn)
-        elif selection == "Profile":
-            show_profile(user_id, conn)
-        elif selection == "Water Intake Tracker":
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Fitness-Tracker',
+                options=['Food','Sleep','Water','Profile','Logout'],
+                icons=['cake-fill','moon-fill','water','person-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'#080808',"color":"#e4e0e0"},# pkc=#eaf8db
+        "icon": {"color": "#1f211c", "font-size": "23px"}, 
+        "nav-link": {"color":"#e4e0e0","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "#1f211c"},
+        "nav-link-selected": {"background-color": "yellow","color":"#1f211c"},} # pdc=fg
+                
+                )
+
+        
+        if app == "Food":
+          food_calorie_tracker(user_id, conn)
+        if app == "Sleep":
+            sleep_quality_tracker(user_id, conn)     
+        if app == 'Water':
             water_intake_tracker(user_id, conn)
-        elif selection == "Logout":
+        if app == "Profile":
+            show_profile(user_id, conn)  
+        if app == 'Logout':
             session_state = st.session_state
             session_state.user_id = None
             st.success("Logout successful")
