989617269b041894005b6a5c6649c5fdf2005426
CHANDNI JHA <96429544+ChandniJha630@users.noreply.github.com>
2024-03-25 13:57:14 +0530

    Update display_navigation.py

--- FrontEnd/display_navigation.py
+++ FrontEnd/display_navigation.py
@@ -6,10 +6,11 @@ from FrontEnd.sleepPage import sleep_quality_tracker
 from FrontEnd.profilePage import show_profile
 from FrontEnd.waterPage import water_intake_tracker
 from FrontEnd.loginPage import user_login
+from FrontEnd.motivation import show_motivational_quote
 from streamlit_option_menu import option_menu
 
 def connect_to_database1():
-    connStr = "system/prakriti1@localhost:1521/xepdb1"
+    connStr = "system/chand123@localhost:1521/xepdb1"
     conn = cx_Oracle.connect(connStr)
     return conn
 
@@ -24,10 +25,10 @@ def navigation(user_id, conn):
         with st.sidebar:        
             app = option_menu(
                 menu_title='Fitness-Tracker',
-                options=['Food','Sleep','Water','Profile','Logout'],
-                icons=['cake-fill','moon-fill','water','person-fill','info-circle-fill'],
+                options=['Profile','Food','Sleep','Water','Get Motivation','Logout'],
+                icons=['person-fill','cake-fill','moon-fill','water','sun-fill','info-circle-fill'],
                 menu_icon='chat-text-fill',
-                default_index=1,
+                default_index=4,
                 styles={
                     "container": {"padding": "5!important","background-color":'#080808',"color":"#e4e0e0"},# pkc=#eaf8db
         "icon": {"color": "#1f211c", "font-size": "23px"}, 
@@ -45,6 +46,8 @@ def navigation(user_id, conn):
             water_intake_tracker(user_id, conn)
         if app == "Profile":
             show_profile(user_id, conn)  
+        if app == "Get Motivation":
+            show_motivational_quote()
         if app == 'Logout':
             session_state = st.session_state
             session_state.user_id = None
